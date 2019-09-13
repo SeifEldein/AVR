3afa102d53bd591b8d11622887e33089c1c6a03b
Seif Eldein <crocodile_thehunter@yahoo.com>
2019-09-13 22:17:57 +0200

    My Libraries

--- My Libraries/MyDIO/MyDIO.c
+++ My Libraries/MyDIO/MyDIO.c
@@ -2,16 +2,15 @@
 #include"Mytype.h"
 #include"MyDIO.h"
 
-void DIO_SetPinDir(U8 PORT,U8 PIN_NUM,U8 PinsRange,U8 IN_OUT)
+void DIO_SetPinDir(U8 PORT,U8 index,U8 PinsRange,U8 IN_OUT)
 {
-	U8 index;
 	switch(PORT)
 	{
 	    case 'A':
 	    {
 	    	for(index=0;index<PinsRange;index++)
 	    	{
-	    	    DIO_DDRA |= (IN_OUT<<PIN_NUM);
+	    	    DIO_DDRA |= (IN_OUT<<index);
 	    	}
 		    break;
 	    }
@@ -19,7 +18,7 @@ void DIO_SetPinDir(U8 PORT,U8 PIN_NUM,U8 PinsRange,U8 IN_OUT)
 	    {
 	    	for(index=0;index<PinsRange;index++)
 	    	{
-	    	    DIO_DDRB |= (IN_OUT<<PIN_NUM);
+	    	    DIO_DDRB |= (IN_OUT<<index);
 	    	}
 		    break;
 	    }
@@ -27,7 +26,7 @@ void DIO_SetPinDir(U8 PORT,U8 PIN_NUM,U8 PinsRange,U8 IN_OUT)
 	    {
 	    	for(index=0;index<PinsRange;index++)
 	    	{
-	    	    DIO_DDRC |= (IN_OUT<<PIN_NUM);
+	    	    DIO_DDRC |= (IN_OUT<<index);
 	    	}
 		    break;
 	    }
@@ -35,70 +34,15 @@ void DIO_SetPinDir(U8 PORT,U8 PIN_NUM,U8 PinsRange,U8 IN_OUT)
 	    {
 	    	for(index=0;index<PinsRange;index++)
 	    	{
-	    	    DIO_DDRD |= (IN_OUT<<PIN_NUM);
+	    	    DIO_DDRD |= (IN_OUT<<index);
 	    	}
 		    break;
 	    }
 	}
 }
 
-void DIO_SetPortVal(U8 PORT,U8 PIN_NUM,U8 HIGH_LOW)
-{
-	switch(PORT)
-	{
-	    case 'A':
-	    {
-	    	if(HIGH_LOW == 1)
-	    	{
-	    	    DIO_PORTA = (HIGH_LOW<<PIN_NUM);
-	    	}
-	    	else
-	    	{
-	    		DIO_PORTA &=~ (HIGH_LOW<<PIN_NUM);
-	    	}
-		    break;
-	    }
-	    case 'B':
-	    {
-	    	if(HIGH_LOW == 1)
-	    	{
-	    	    DIO_PORTB = (HIGH_LOW<<PIN_NUM);
-	    	}
-	    	else
-	    	{
-	    		DIO_PORTB &=~ (HIGH_LOW<<PIN_NUM);
-	    	}
-		    break;
-	    }
-	    case 'C':
-	    {
-	    	if(HIGH_LOW == 1)
-	    	{
-	    	    DIO_PORTC = (HIGH_LOW<<PIN_NUM);
-	    	}
-	    	else
-	    	{
-	    		DIO_PORTC &=~ (HIGH_LOW<<PIN_NUM);
-	    	}
-		    break;
-	    }
-	    case 'D':
-	    {
-	    	if(HIGH_LOW == 1)
-	    	{
-	    	    DIO_PORTD = (HIGH_LOW<<PIN_NUM);
-	    	}
-	    	else
-	    	{
-	    		DIO_PORTD &=~ (HIGH_LOW<<PIN_NUM);
-	    	};
-		    break;
-	    }
-	}
 
-}
-
-void DIO_SetPortVal(U8 PORT,U8 PIN_NUM,U8 HIGH_LOW)
+void DIO_SetPinVal(U8 PORT,U8 PIN_NUM,U8 HIGH_LOW)
 {
 	switch(PORT)
 	{
