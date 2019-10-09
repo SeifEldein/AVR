d1396b5afc7b3ffdd5584c525adad9236182e9a5
SeifEldein <54726314+SeifEldein@users.noreply.github.com>
2019-10-10 00:18:12 +0200

    Add files via upload

--- My Libraries/LCD Driver/LCDD.c
+++ My Libraries/LCD Driver/LCDD.c
@@ -7,18 +7,15 @@
 void LCD_Init(void)
 {
 	_delay_ms(60);
+	DIO_SetPinDir('D',LCD_DB0_PIN,LCD_DB7_PIN,DIO_HIGH);  /* PORT D OUTPUT */
+	DIO_SetPinDir('B',LCD_DB0_PIN,LCD_DB2_PIN,DIO_HIGH);  /* PORT B OUTPUT */
+	DIO_SetPortVal('B',LCD_RS,DIO_LOW);                   /* RS = 0 */
+	DIO_SetPortVal('B',LCD_RW,DIO_LOW);                   /* RW = 0 */
+	DIO_SetPortVal('B',LCD_EN,DIO_LOW);                   /* EN = 0 */
 
-	DIO_SetPinDir('D',LCD_DB0_PIN,LCD_DB7_PIN,DIO_HIGH);   // Set Port D as output.
-	DIO_SetPinDir('B',LCD_DB0_PIN,LCD_DB2_PIN,DIO_HIGH);   // Set Port B as output.
-
-	DIO_SetPortVal('B',LCD_RS,DIO_LOW);                /* RS = 0 */
-	DIO_SetPortVal('B',LCD_RW,DIO_LOW);                /* RW = 0 */
-
-	LCD_Instruction = _8bit_mode;
-	LCD_Instruction = Second_Line;
-	LCD_Instruction = Cursor_Increment;
-	LCD_Instruction = Dis_clr;
-	LCD_Instruction = 0x80;
+	LCD_Ctrl(_8bit_mode);
+	LCD_Ctrl(screen_cursor_ON);
+	LCD_Ctrl(Cursor_Increment);
 
     _delay_ms(5);
 }
@@ -29,7 +26,7 @@ void LCD_Ctrl(U8 Instruction)
 	DIO_SetPortVal('B',LCD_RS,DIO_LOW);                /* RS = 0 */
 	DIO_SetPortVal('B',LCD_RW,DIO_LOW);                /* RW = 0 */
 	DIO_SetPortVal('B',LCD_EN,DIO_HIGH);               /* EN = 1 */
-	_delay_us(1);
+	_delay_ms(1);
 	DIO_SetPortVal('B',LCD_EN,DIO_LOW);                /* EN = 0 */
 
 	_delay_ms(5);
@@ -47,7 +44,7 @@ void LCD_Data(U8 Character)
 	_delay_ms(5);
 }
 
-void LCD_String(S8 *S)
+void LCD_String(S8 S[])
 {
 	S8 i;
 	for(i=0;S[i]!=0;i++)
